12a7da12cee215e6dd2e9a7514e495ab5611a7ab
palerdot <palerdot@gmail.com>
2024-01-31 23:14:23 +0530

    fix: inlining files with rust crate

--- src/wordle/data.rs
+++ src/wordle/data.rs
@@ -1,22 +1,13 @@
-use std::fs;
-
-fn parse_file(path: String) -> Vec<String> {
-    let file_data = fs::read_to_string(path.clone());
-
-    if file_data.is_err() {
-        panic!("cannot read file {}", path.clone());
-    }
-
-    let file_data = file_data.unwrap();
+pub fn valid_guesses() -> Vec<String> {
+    let file_data = include_str!("./guess.txt");
     let words = file_data.split("\n");
 
     words.map(|x| x.to_string()).collect()
 }
 
-pub fn valid_guesses() -> Vec<String> {
-    parse_file("./data/guess.txt".to_string())
-}
-
 pub fn valid_wordles() -> Vec<String> {
-    parse_file("./data/answer.txt".to_string())
+    let file_data = include_str!("./answer.txt");
+    let words = file_data.split("\n");
+
+    words.map(|x| x.to_string()).collect()
 }
