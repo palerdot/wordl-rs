c8e4c0b5180555f480bafc526c6a8a4e783914fc
palerdot <palerdot@gmail.com>
2024-02-01 09:04:37 +0530

    moving data to files directory

--- src/wordle/data.rs
+++ src/wordle/data.rs
@@ -1,12 +1,12 @@
 pub fn valid_guesses() -> Vec<String> {
-    let file_data = include_str!("./guess.txt");
+    let file_data = include_str!("./files/guess.txt");
     let words = file_data.split("\n");
 
     words.map(|x| x.to_string()).collect()
 }
 
 pub fn valid_wordles() -> Vec<String> {
-    let file_data = include_str!("./answer.txt");
+    let file_data = include_str!("./files/answer.txt");
     let words = file_data.split("\n");
 
     words.map(|x| x.to_string()).collect()
